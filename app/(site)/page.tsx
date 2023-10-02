ccc3ac2d47fcb75e155774cb925d71d29b221fcd
Valeriy <masterbox06@gmail.com>
2023-10-02 17:58:46 +0300

    removed gap

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,9 @@
 import {WelcomeSec} from "@/app/(site)/components/WelcomeSec";
 
 export default function Home() {
-  console.log("rendering home")
   return (
     <main>
-      <section className="flex h-screen flex-col items-center justify-center gap-8">
+      <section className="flex h-screen flex-col items-center justify-center">
         <WelcomeSec/>
       </section>
     </main>
