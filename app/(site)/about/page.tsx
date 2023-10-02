0ca5b37b6e35b6de6e4ef5d3546dc942db465b16
Valeriy <masterbox06@gmail.com>
2023-10-02 13:06:22 +0300

    added temp pages

---
+++ app/(site)/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <section className="flex min-h-screen flex-col items-center justify-center gap-8 bg-gradient-to-r from-sky-500 to-indigo-500">
+      <h1 className="text-5xl font-bold">About</h1>
+      <p className="mt-3 text-2xl">This page is about me!</p>
+    </section>
+  );
+}
