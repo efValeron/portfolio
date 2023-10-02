0ec3653e68d85ed573fbb0f38dbcf8ed35a57472
Valeriy <masterbox06@gmail.com>
2023-10-02 18:00:05 +0300

    added skills section, layout changed to row

--- app/(site)/components/WelcomeSec.tsx
+++ app/(site)/components/WelcomeSec.tsx
@@ -1,15 +1,18 @@
 "use client"
 
-import {motion} from "framer-motion";
-import {useEffect, useState} from "react";
-import {ProfileType} from "@/types";
-import {getProfile} from "@/sanity/sanity.query";
+import {motion} from "framer-motion"
+import {useEffect, useState} from "react"
+import {Profile} from "@/types"
+import {getProfile} from "@/sanity/sanity.query"
+import Image from "next/image"
+import {getSanitySvgImageUrl} from "@/utils"
+import Link from "next/link"
 
 export const WelcomeSec = () => {
-  const [profile, setProfile] = useState<ProfileType | null>(null);
+  const [profile, setProfile] = useState<Profile | null>(null)
 
   useEffect(() => {
-    getProfileHandle().then(profile => setProfile(profile[0]));
+    getProfileHandle().then(profile => setProfile(profile[0]))
   }, [])
 
   const getProfileHandle = async () => await getProfile()
@@ -18,23 +21,60 @@ export const WelcomeSec = () => {
     <>
       {
         profile &&
-        <>
+        <div className="flex gap-32 items-center">
+          <div className="text-left">
           <motion.h1
             initial={{opacity: 0, y: 20}}
             animate={{opacity: 1, y: 0}}
             transition={{duration: 1}}
-            className="text-5xl font-bold drop-shadow-light-glow dark:drop-shadow-dark-glow">Hi there👋,
-            I'm {profile.fullName}!
+            className="text-5xl line- font-bold drop-shadow-light-glow dark:drop-shadow-dark-glow">
+            Hi there👋, I'm {profile.fullName}
           </motion.h1>
+          <motion.h2
+            initial={{opacity: 0, y: 20}}
+            animate={{opacity: 1, y: 0}}
+            transition={{duration: 1}}
+            className="text-4xl mt-4 font-bold drop-shadow-light-glow dark:drop-shadow-dark-glow">
+            Frond-end Developer!
+          </motion.h2>
           <motion.p
             initial={{opacity: 0, y: 20}}
             animate={{opacity: 1, y: 0}}
-            transition={{duration: 1, delay: 0.5}}
-            className="mt-8 text-center text-2xl drop-shadow-light-glow dark:drop-shadow-dark-glow">{profile.headline}
+            transition={{duration: 1, delay: 0.25}}
+            className="mt-10 text-2xl drop-shadow-light-glow dark:drop-shadow-dark-glow">{profile.headline}
           </motion.p>
-        </>
+          </div>
+          <div className="mt-8 gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            <SkillsSection profile={profile}/>
+          </div>
+        </div>
       }
     </>
   )
-    ;
-};
+}
+
+const SkillsSection = ({profile}: {
+  profile: Profile
+}) => {
+  return profile.skills.map((skill, index) =>
+    // console.log(skill.theme === undefined || skill.theme === theme ? skill.title : false)
+    skill.theme === undefined || skill.theme === "dark"
+      ? <motion.div
+        initial={{opacity: 0, y: 20}}
+        // initial={{opacity: 0, y: index % 2 === 0 ? 5 : -5,}}
+        animate={{opacity: 1, y: 0}}
+        transition={{duration: 0.75, delay: index * 0.05}}
+        key={skill._key}
+      >
+        <Link href={skill.homePageUrl} target="_blenk">
+          <Image
+            src={getSanitySvgImageUrl(skill.image.asset._ref)}
+            alt={skill.title}
+            width={40}
+            height={40}
+          />
+        </Link>
+      </motion.div>
+      : null
+  )
+}
