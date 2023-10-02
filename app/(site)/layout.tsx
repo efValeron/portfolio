30b0e590d71f78355bbf66a038ea36c1f0af1746
Valeriy <masterbox06@gmail.com>
2023-10-02 13:07:55 +0300

    changed metadata, added nav and providers

---
+++ app/(site)/layout.tsx
@@ -0,0 +1,29 @@
+import '../globals.css'
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
+import {Providers} from "@/app/(site)/providers";
+import {Nav} from "@/app/(site)/components/Nav";
+
+const inter = Inter({subsets: ['latin']})
+
+export const metadata: Metadata = {
+  title: 'Portfolio',
+  description: 'Next Generation Portfolio',
+}
+
+export default function RootLayout({
+                                     children,
+                                   }: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+    <body className={inter.className}>
+    <Providers>
+      <Nav/>
+      {children}
+    </Providers>
+    </body>
+    </html>
+  )
+}
