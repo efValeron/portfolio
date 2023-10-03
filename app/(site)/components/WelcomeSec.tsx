4890e4f9db9d3316a9faf8a5828cbbfa40696c08
Valeriy <masterbox06@gmail.com>
2023-10-04 00:01:33 +0300

    added short bio and social links, some styles improvement, added test cards, removed skill section

--- app/(site)/components/WelcomeSec.tsx
+++ app/(site)/components/WelcomeSec.tsx
@@ -3,11 +3,13 @@
 import {motion} from "framer-motion"
 import {useEffect, useState} from "react"
 import {Profile} from "@/types"
-import {getProfile} from "@/sanity/sanity.query"
+import {getProfileForHome} from "@/sanity/sanity.query"
 import Image from "next/image"
 import {getSanitySvgImageUrl} from "@/utils"
-import Link from "next/link"
-import { TypeAnimation } from 'react-type-animation'
+import {Link} from "@nextui-org/react";
+import NextLink from "next/link";
+import {TypeAnimation} from 'react-type-animation'
+import {HomeCard} from "@/app/(site)/components/HomeCard";
 
 export const WelcomeSec = () => {
   const [profile, setProfile] = useState<Profile | null>(null)
@@ -16,50 +18,70 @@ export const WelcomeSec = () => {
     getProfileHandle().then(profile => setProfile(profile[0]))
   }, [])
 
-  const getProfileHandle = async () => await getProfile()
+  const getProfileHandle = async () => await getProfileForHome()
 
   return (
     <>
       {
         profile &&
-        <div className="flex gap-32 items-center">
-          <div className="text-left">
-          <motion.h1
-            initial={{opacity: 0, y: 20}}
-            animate={{opacity: 1, y: 0}}
-            transition={{duration: 1}}
-            className="text-6xl welcome-heading">
-            Hi there👋, I'm
-          </motion.h1>
-          <motion.h2
-            initial={{opacity: 0, y: 20}}
-            animate={{opacity: 1, y: 0}}
-            transition={{duration: 1}}
-            className="text-5xl mt-4 welcome-heading">
-            <TypeAnimation
-              sequence={[
-                1000,
-                profile.fullName,
-                1000,
-                "Front-End Developer",
-              ]}
-              wrapper="span"
-              speed={10}
-              deletionSpeed={50}
-              // preRenderFirstString
-              repeat={Infinity}
-            />
-          </motion.h2>
-          <motion.p
-            initial={{opacity: 0, y: 20}}
-            animate={{opacity: 1, y: 0}}
-            transition={{duration: 1, delay: 0.25}}
-            className="mt-10 text-3xl drop-shadow-light-glow dark:drop-shadow-dark-glow">
-            {profile.headline}
-          </motion.p>
+        <div className="flex gap-32 items-center container">
+          <div className="text-left w-1/2">
+            <motion.h1
+              initial={{opacity: 0, y: 20}}
+              animate={{opacity: 1, y: 0}}
+              transition={{duration: 1}}
+              className="text-6xl font-extrabold glowing">
+              Hi there👋, I'm
+            </motion.h1>
+            <motion.h2
+              initial={{opacity: 0, y: 20}}
+              animate={{opacity: 1, y: 0}}
+              transition={{duration: 1}}
+              className="text-5xl font-extrabold mt-4 glowing text-primary-600">
+              <TypeAnimation
+                sequence={[
+                  1000,
+                  profile.fullName,
+                  1000,
+                  profile.specialization,
+                ]}
+                wrapper="span"
+                speed={10}
+                deletionSpeed={50}
+                preRenderFirstString
+                repeat={Infinity}
+              />
+            </motion.h2>
+            <motion.p
+              initial={{opacity: 0, y: 20}}
+              animate={{opacity: 1, y: 0}}
+              transition={{duration: 1, delay: 0.25}}
+              className="mt-10 text-3xl font-bold">
+              {profile.headline}
+            </motion.p>
+
+            <motion.p
+              initial={{opacity: 0, y: 20}}
+              animate={{opacity: 0.75, y: 0}}
+              transition={{duration: 1, delay: 0.5}}
+              className="mt-10 w-4/5">
+              {profile.shortBio}
+            </motion.p>
+
+            <motion.div
+              initial={{opacity: 0, y: 20}}
+              animate={{opacity: 1, y: 0}}
+              transition={{duration: 1, delay: 0.75}}
+              className="mt-10 flex gap-2">
+              <SocialLinksSection socialLinks={profile.socialLinks}/>
+            </motion.div>
           </div>
-          <div className="mt-8 gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-            <SkillsSection profile={profile}/>
+          <div className="gap-8 grid grid-cols-1 sm:grid-cols-2">
+            {/*<SkillsSection skills={profile.skills}/>*/}
+            <HomeCard/>
+            <HomeCard/>
+            <HomeCard/>
+            <HomeCard/>
           </div>
         </div>
       }
@@ -67,28 +89,10 @@ export const WelcomeSec = () => {
   )
 }
 
-const SkillsSection = ({profile}: {
-  profile: Profile
-}) => {
-  return profile.skills.map((skill, index) =>
-    // console.log(skill.theme === undefined || skill.theme === theme ? skill.title : false)
-    skill.theme === undefined || skill.theme === "dark"
-      ? <motion.div
-        initial={{opacity: 0, y: 20}}
-        // initial={{opacity: 0, y: index % 2 === 0 ? 5 : -5,}}
-        animate={{opacity: 1, y: 0}}
-        transition={{duration: 0.75, delay: index * 0.05}}
-        key={skill._key}
-      >
-        <Link href={skill.homePageUrl} target="_blenk">
-          <Image
-            src={getSanitySvgImageUrl(skill.image.asset._ref)}
-            alt={skill.title}
-            width={40}
-            height={40}
-          />
-        </Link>
-      </motion.div>
-      : null
+const SocialLinksSection = ({socialLinks}: { socialLinks: Profile["socialLinks"] }) => {
+  return socialLinks.map((link) =>
+    <Link key={link._key} isBlock showAnchorIcon color="primary" href={link.url} target="_blank" as={NextLink}>
+      {link.title}
+    </Link>
   )
 }
