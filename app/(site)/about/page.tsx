e5ee5bcdb0c38ae191a0abd2fb47b0ac240419db
Valeriy <masterbox06@gmail.com>
2023-10-04 14:27:48 +0300

    added section to about page

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -1,8 +1,11 @@
+import {AboutSec} from "@/app/(site)/components/AboutSec";
+
 export default function About() {
   return (
-    <section className="flex min-h-screen flex-col items-center justify-center gap-8 bg-gradient-to-r from-sky-500 to-indigo-500">
-      <h1 className="text-5xl font-bold">About</h1>
-      <p className="mt-3 text-2xl">This page is about me!</p>
-    </section>
+    <main>
+      <section className="flex min-h-screen py-32 flex-col items-center justify-center">
+        <AboutSec/>
+      </section>
+    </main>
   );
 }
