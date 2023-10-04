9d7e83e2935478899ef9600fb0b4908ace3611d2
Valeriy <masterbox06@gmail.com>
2023-10-04 14:25:53 +0300

    added ease, social links color changed to foreground, skills returned

--- app/(site)/components/WelcomeSec.tsx
+++ app/(site)/components/WelcomeSec.tsx
@@ -4,12 +4,11 @@ import {motion} from "framer-motion"
 import {useEffect, useState} from "react"
 import {Profile} from "@/types"
 import {getProfileForHome} from "@/sanity/sanity.query"
-import Image from "next/image"
-import {getSanitySvgImageUrl} from "@/utils"
 import {Link} from "@nextui-org/react";
 import NextLink from "next/link";
 import {TypeAnimation} from 'react-type-animation'
-import {HomeCard} from "@/app/(site)/components/HomeCard";
+import {Skills} from "@/app/(site)/components/Skills";
+import {ExternalLink} from "lucide-react";
 
 export const WelcomeSec = () => {
   const [profile, setProfile] = useState<Profile | null>(null)
@@ -24,19 +23,19 @@ export const WelcomeSec = () => {
     <>
       {
         profile &&
-        <div className="flex gap-32 items-center container">
+        <div className="flex gap-32 items-center container px-44">
           <div className="text-left w-1/2">
             <motion.h1
               initial={{opacity: 0, y: 20}}
               animate={{opacity: 1, y: 0}}
-              transition={{duration: 1}}
+              transition={{duration: 1, ease: "backOut"}}
               className="text-6xl font-extrabold glowing">
               Hi there👋, I'm
             </motion.h1>
             <motion.h2
               initial={{opacity: 0, y: 20}}
               animate={{opacity: 1, y: 0}}
-              transition={{duration: 1}}
+              transition={{duration: 1, ease: "backOut"}}
               className="text-5xl font-extrabold mt-4 glowing text-primary-600">
               <TypeAnimation
                 sequence={[
@@ -55,15 +54,15 @@ export const WelcomeSec = () => {
             <motion.p
               initial={{opacity: 0, y: 20}}
               animate={{opacity: 1, y: 0}}
-              transition={{duration: 1, delay: 0.25}}
+              transition={{duration: 1, delay: 0.25, ease: "backOut"}}
               className="mt-10 text-3xl font-bold">
               {profile.headline}
             </motion.p>
 
             <motion.p
               initial={{opacity: 0, y: 20}}
-              animate={{opacity: 0.75, y: 0}}
-              transition={{duration: 1, delay: 0.5}}
+              animate={{opacity: 0.85, y: 0}}
+              transition={{duration: 1, delay: 0.5, ease: "backOut"}}
               className="mt-10 w-4/5">
               {profile.shortBio}
             </motion.p>
@@ -71,17 +70,18 @@ export const WelcomeSec = () => {
             <motion.div
               initial={{opacity: 0, y: 20}}
               animate={{opacity: 1, y: 0}}
-              transition={{duration: 1, delay: 0.75}}
+              transition={{duration: 1, delay: 0.75, ease: "backOut"}}
               className="mt-10 flex gap-2">
-              <SocialLinksSection socialLinks={profile.socialLinks}/>
+              <SocialLinks socialLinks={profile.socialLinks}/>
             </motion.div>
           </div>
-          <div className="gap-8 grid grid-cols-1 sm:grid-cols-2">
-            {/*<SkillsSection skills={profile.skills}/>*/}
-            <HomeCard/>
-            <HomeCard/>
-            <HomeCard/>
-            <HomeCard/>
+          {/*<div className="gap-8 grid grid-cols-1 sm:grid-cols-2">*/}
+          <div className="gap-3 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+            <Skills skills={profile.skills}/>
+            {/*<HomeCard/>*/}
+            {/*<HomeCard/>*/}
+            {/*<HomeCard/>*/}
+            {/*<HomeCard/>*/}
           </div>
         </div>
       }
@@ -89,9 +89,9 @@ export const WelcomeSec = () => {
   )
 }
 
-const SocialLinksSection = ({socialLinks}: { socialLinks: Profile["socialLinks"] }) => {
+const SocialLinks = ({socialLinks}: { socialLinks: Profile["socialLinks"] }) => {
   return socialLinks.map((link) =>
-    <Link key={link._key} isBlock showAnchorIcon color="primary" href={link.url} target="_blank" as={NextLink}>
+    <Link key={link._key} isBlock showAnchorIcon color="foreground" href={link.url} target="_blank" as={NextLink}>
       {link.title}
     </Link>
   )
