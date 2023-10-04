16a311ea9c6eb266f6302fe7931f6fcf976480b1
Valeriy <masterbox06@gmail.com>
2023-10-04 14:24:19 +0300

    primary color changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,22 @@
 import type {Config} from 'tailwindcss'
 import {nextui} from "@nextui-org/react"
 
+const overrideColors = {
+  primary: {
+    DEFAULT: "#F5A524",
+    50: "#FEFCE8",
+    100: "#FDEDD3",
+    200: "#FBDBA7",
+    300: "#F9C97C",
+    400: "#F7B750",
+    500: "#F5A524",
+    600: "#C4841D",
+    700: "#936316",
+    800: "#62420E",
+    900: "#312107"
+  }
+}
+
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -30,12 +46,14 @@ const config: Config = {
           colors: {
             background: "#ECEDEE", // or DEFAULT
             foreground: "#18181b", // or 50 to 900 DEFAULT
+            ...overrideColors
           }
         },
         dark: {
           colors: {
             background: "#18181b", // or DEFAULT
             foreground: "#ECEDEE", // or 50 to 900 DEFAULT
+            ...overrideColors
           }
         },
       }
