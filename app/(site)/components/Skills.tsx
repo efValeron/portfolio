3a4a017487c9ba46411ed8971c92d173bf41e9c4
Valeriy <masterbox06@gmail.com>
2023-10-05 17:22:07 +0300

    added motion delay

--- app/(site)/components/Skills.tsx
+++ app/(site)/components/Skills.tsx
@@ -11,7 +11,7 @@ export const Skills = ({skills}: { skills: Profile["skills"] }) => {
       ? <motion.div
         initial={{opacity: 0, y: 20}}
         animate={{opacity: 1, y: 0}}
-        transition={{duration: 0.75, delay: index * 0.05, ease: "backOut"}}
+        transition={{duration: 0.75, delay: index * 0.05 + 1.75, ease: "backOut"}}
         key={skill._key}
         className="w-[40px] h-[40px]"
       >
@@ -19,6 +19,8 @@ export const Skills = ({skills}: { skills: Profile["skills"] }) => {
           <Image
             src={getSanitySvgImageUrl(skill.image.asset._ref)}
             alt={skill.title}
+            // layout='fill'
+            // className="w-[40px] h-[40px]"
             width={40}
             height={40}
           />
