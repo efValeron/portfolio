1956f3058f101a758821e2482abfa6e9d6e158bd
Valeriy <masterbox06@gmail.com>
2023-10-05 17:24:23 +0300

    added small devises menu, brand changed

--- app/(site)/components/Nav.tsx
+++ app/(site)/components/Nav.tsx
@@ -1,12 +1,14 @@
 "use client"
 
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, NavbarMenuToggle, NavbarMenu} from "@nextui-org/react";
 import {ThemeSwitcher} from "@/app/(site)/components/ThemeSwitcher";
 import NextLink from "next/link";
 import {usePathname} from "next/navigation";
+import {useState} from "react";
 
 export const Nav = () => {
   const pathname = usePathname();
+  const [isMenuOpen] = useState(false);
 
   const isHome = pathname === "/"
   const isAbout = pathname === "/about"
@@ -15,8 +17,12 @@ export const Nav = () => {
 
   return (
     <Navbar className="fixed top-0 left-0 right-0 z-50" isBordered>
+      <NavbarMenuToggle
+        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        className="sm:hidden"
+      />
       <NavbarBrand>
-        <p className="font-bold text-inherit">ACME</p>
+        <p className="font-bold text-inherit">DEValeron</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-6" justify="center">
         <NavbarItem isActive={isHome}>
@@ -43,6 +49,43 @@ export const Nav = () => {
       <NavbarContent justify="end">
         <ThemeSwitcher/>
       </NavbarContent>
+
+      <NavbarMenu className="overflow-y-hidden">
+        {/*{menuItems.map((item, index) => (*/}
+        {/*  <NavbarMenuItem key={`${item}-${index}`}>*/}
+        {/*    <Link*/}
+        {/*      color={*/}
+        {/*        index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"*/}
+        {/*      }*/}
+        {/*      className="w-full"*/}
+        {/*      href="#"*/}
+        {/*      size="lg"*/}
+        {/*    >*/}
+        {/*      {item}*/}
+        {/*    </Link>*/}
+        {/*  </NavbarMenuItem>*/}
+        {/*))}*/}
+        <NavbarItem isActive={isHome}>
+          <Link isBlock color={isHome ? "primary" : "foreground"} href="/" as={NextLink}>
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive={isAbout}>
+          <Link isBlock color={isAbout ? "primary" : "foreground"} href="/about" as={NextLink}>
+            About
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive={isProjects}>
+          <Link isBlock color={isProjects ? "primary" : "foreground"} href="/projects" as={NextLink}>
+            Projects
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive={isContact}>
+          <Link isBlock color={isContact ? "primary" : "foreground"} href="/contact" as={NextLink}>
+            Contact
+          </Link>
+        </NavbarItem>
+      </NavbarMenu>
     </Navbar>
   );
 };
