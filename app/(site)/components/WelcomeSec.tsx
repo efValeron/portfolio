396d481bc651d4387d5360efb9ab89ed4675fbb8
Valeriy <masterbox06@gmail.com>
2023-10-05 17:25:36 +0300

    added responsiveness, counter and skills sections

--- app/(site)/components/WelcomeSec.tsx
+++ app/(site)/components/WelcomeSec.tsx
@@ -8,7 +8,7 @@ import {Link} from "@nextui-org/react";
 import NextLink from "next/link";
 import {TypeAnimation} from 'react-type-animation'
 import {Skills} from "@/app/(site)/components/Skills";
-import {ExternalLink} from "lucide-react";
+import {Interval} from "@/app/(site)/components/Interval";
 
 export const WelcomeSec = () => {
   const [profile, setProfile] = useState<Profile | null>(null)
@@ -23,20 +23,20 @@ export const WelcomeSec = () => {
     <>
       {
         profile &&
-        <div className="flex gap-32 items-center container px-44">
-          <div className="text-left w-1/2">
+        <div className="flex gap-10 lg:gap-20 xl:gap-32 container lg:px-20 xl:px-32 flex-col lg:flex-row">
+          <div className="px-8 md:px-16 lg:w-1/2 lg:px-0">
             <motion.h1
               initial={{opacity: 0, y: 20}}
               animate={{opacity: 1, y: 0}}
               transition={{duration: 1, ease: "backOut"}}
-              className="text-6xl font-extrabold glowing">
+              className="text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl font-extrabold glowing">
               Hi there👋, I'm
             </motion.h1>
             <motion.h2
               initial={{opacity: 0, y: 20}}
               animate={{opacity: 1, y: 0}}
               transition={{duration: 1, ease: "backOut"}}
-              className="text-5xl font-extrabold mt-4 glowing text-primary-600">
+              className="text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl font-extrabold mt-2 lg:mt-4 glowing text-primary-600">
               <TypeAnimation
                 sequence={[
                   1000,
@@ -51,19 +51,19 @@ export const WelcomeSec = () => {
                 repeat={Infinity}
               />
             </motion.h2>
-            <motion.p
+            <motion.h3
               initial={{opacity: 0, y: 20}}
               animate={{opacity: 1, y: 0}}
               transition={{duration: 1, delay: 0.25, ease: "backOut"}}
-              className="mt-10 text-3xl font-bold">
+              className="mt-5 lg:mt-10 text-md md:text-lg lg:text-xl xl:text-2xl font-bold">
               {profile.headline}
-            </motion.p>
+            </motion.h3>
 
             <motion.p
               initial={{opacity: 0, y: 20}}
               animate={{opacity: 0.85, y: 0}}
               transition={{duration: 1, delay: 0.5, ease: "backOut"}}
-              className="mt-10 w-4/5">
+              className="mt-5 lg:mt-10">
               {profile.shortBio}
             </motion.p>
 
@@ -71,17 +71,34 @@ export const WelcomeSec = () => {
               initial={{opacity: 0, y: 20}}
               animate={{opacity: 1, y: 0}}
               transition={{duration: 1, delay: 0.75, ease: "backOut"}}
-              className="mt-10 flex gap-2">
+              className="mt-5 md:mt-10 flex gap-2 flex-col md:flex-row">
               <SocialLinks socialLinks={profile.socialLinks}/>
             </motion.div>
           </div>
-          {/*<div className="gap-8 grid grid-cols-1 sm:grid-cols-2">*/}
-          <div className="gap-3 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-            <Skills skills={profile.skills}/>
-            {/*<HomeCard/>*/}
-            {/*<HomeCard/>*/}
-            {/*<HomeCard/>*/}
-            {/*<HomeCard/>*/}
+
+          <div className="px-8 lg:self-start lg:w-1/2 lg:px-0 box-border">
+            <motion.h3
+              initial={{opacity: 0, y: 20}}
+              animate={{opacity: 1, y: 0}}
+              transition={{duration: 1, delay: 1, ease: "backOut"}}
+              className="text-xl lg:text-2xl font-semibold text-center mb-6 lg:mb-8">Time since I started <span
+              className="text-gradient font-extrabold">experiencing</span>:
+            </motion.h3>
+            <Interval/>
+
+            <motion.h4
+              initial={{opacity: 0, y: 20}}
+              animate={{opacity: 1, y: 0}}
+              transition={{duration: 1, delay: 1.5, ease: "backOut"}}
+              className="text-lg lg:text-xl font-medium text-center mt-10 mb-5 lg:mt-12 lg:mb-8"
+            >These pictures
+              seem familiar to me:
+            </motion.h4>
+            <div className="flex justify-center">
+              <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 gap-2">
+                <Skills skills={profile.skills}/>
+              </div>
+            </div>
           </div>
         </div>
       }
