c363180db150be60ee3ccadff013576b6c46a25c
efValeron <masterbox06@gmail.com>
2023-12-15 18:28:59 +0600

    feat: add NextJS Speed Insights

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,18 +1,19 @@
-import '../globals.css'
-import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
-import {Providers} from "@/app/(site)/providers";
-import {Nav} from "@/app/(site)/components/Nav";
+import "../globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { Providers } from "@/app/(site)/providers"
+import { Nav } from "@/app/(site)/components/Nav"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: [ "latin" ] })
 
 export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'Next Generation Portfolio',
+  title: "Portfolio",
+  description: "Next Generation Portfolio"
 }
 
 export default function RootLayout({
-                                     children,
+                                     children
                                    }: {
   children: React.ReactNode
 }) {
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
     <body className={`${inter.className} ${process.env.NODE_ENV === "development" ? "debug-screens" : ""}`}>
     <Providers>
+      <SpeedInsights/>
       <Nav/>
       {children}
     </Providers>
